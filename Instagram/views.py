ec7fcfeea48c52a558433d6d861bbbf859ff6016
Ático <atico@202C11E26168967.local>
2017-10-28 13:27:17 -0500

    segundo commit

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from Instagram.models import *
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method== 'GET':
+        return render(request,'index.html')
+    else:
+        name=request.POST['nombre_completo']
+        username= request.POST ['usuario']
+        email =request.POST['password']
+        password= request.POST['password']
+        print(name)
+        print(username)
+        print(email)
+        print(password)
+
+        usuarioDjango=User.objects.create_user(username = username, password = password, email=email, first_name=name)
+        miUsuario= MiUsuario( usuario_django = usuarioDjango )
+        usuarioDjango.save()
+        miUsuario.save()
+        return redirect('login')
+def login(request):
+    print (MiUsuario.objects.count())
+    return render(request,'home.html')
